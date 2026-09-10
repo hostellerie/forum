66c722b612ad73014eae28999619d0c3caa3c5c2
Ben <hostellerie.org@gmail.com>
2026-09-10 14:44:10 +0200

    Add one-time French header restoration helper

---
+++ tools/restore-french-header.php
@@ -0,0 +1,56 @@
+<?php
+$path = __DIR__ . '/../language/french_france_utf-8.php';
+$contents = file_get_contents($path);
+if ($contents === false) {
+    fwrite(STDERR, "Unable to read French language file\n");
+    exit(1);
+}
+$marker = "global $LANG32;";
+$pos = strpos($contents, $marker);
+if ($pos === false) {
+    fwrite(STDERR, "Unable to locate language body marker\n");
+    exit(1);
+}
+$body = substr($contents, $pos);
+$header = <<<'PHP'
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +---------------------------------------------------------------------------+
+// | Geeklog Forums Plugin 2.9.1                                               |
+// +---------------------------------------------------------------------------+
+// | french_france_utf-8.php                                                   |
+// | Language defines for all text                                             |
+// +---------------------------------------------------------------------------+
+// | Copyright (C) 2011 by the following authors:                              |
+// |    Geeklog Community Members   geeklog-forum AT googlegroups DOT com      |
+// |                                                                           |
+// | Copyright (C) 2000,2001 by the following authors:                         |
+// |    Tony Bibbs       tony AT tonybibbs DOT com                             |
+// |                                                                           |
+// | forum Plugin Authors                                                      |
+// |    Mr.GxBlock                                        www.gxblock.com      |
+// |    Matthew DeWyer   matt AT mycws DOT com            www.cweb.ws          |
+// |    Blaine Lang      geeklog AT langfamily DOT ca     www.langfamily.ca    |
+// |                                                                           |
+// | Translation Author :                                                      |
+// |    David Gaussinel  geeklog AT wipsa DOT net                              |
+// |    Ben              ben AT geeklog DOT fr                                 |
+// +---------------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or             |
+// | modify it under the terms of the GNU General Public License               |
+// | as published by the Free Software Foundation; either version 2            |
+// | of the License, or (at your option) any later version.                    |
+// |                                                                           |
+// | This program is distributed in the hope that it will be useful,           |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             |
+// | GNU General Public License for more details.                              |
+// |                                                                           |
+// | You should have received a copy of the GNU General Public License         |
+// | along with this program; if not, write to the Free Software Foundation,   |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.           |
+// +---------------------------------------------------------------------------+
+
+PHP;
+file_put_contents($path, $header . $body);
